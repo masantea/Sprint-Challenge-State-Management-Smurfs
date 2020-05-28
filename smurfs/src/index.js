19df0fe018244e8251113486671a67b462d024bd
masantea <mercyansong@gmail.com>
2020-05-28 02:47:01 -0400

    index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
